1c50511cda3c5218aa9aa52f0b333a41f9c1a145
Zicheng.S <zichengshan@gmail.com>
2022-05-18 11:31:06 -0700

    Set up client
    index.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Typography, AppBar} from '@material-ui/core'
+
+function App() {
+    return (
+        <div>
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Video Chat App</Typography>
+            </AppBar>
+        </div>
+    )
+}
+
+export default App
